912dd901de97b811f8645c3ed2cfa0388f313ee0
wyq-mac <wangyaqi.chn@gmail.com>
2018-11-30 22:54:42 +0800

    回复评论

--- components/comment.vue
+++ components/comment.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="comment">
+    <!-- 评论列表 -->
     <section class="comment-list">
       <div class="header">
         共
@@ -12,10 +13,21 @@
         <div class="user">
           <b>{{ item.user.name }}</b>
         </div>
-        <div class="content">{{ item.content }}</div>
+        <div class="content">
+          <div 
+            v-if="!!item.pid" 
+            class="reply">
+            <div class="reply-user">@{{ findReplyParent(item.pid, 1) }}</div>
+            <div class="reply-content">{{ findReplyParent(item.pid, 2) }}</div>
+          </div>
+          <div>
+            {{ item.content }}
+          </div>
+        </div>
         <div @click="reply(item._id)">回复</div>
       </div>
     </section>
+    <!-- 评论框 -->
     <section class="comment-form">
       <div class="user">
         <input
@@ -32,12 +44,17 @@
           type="text" 
           placeholder="网站(选填)">
       </div>
-      <div class="reply">
+      <div
+        v-if="!!pid"
+        class="reply">
         <div class="reply-user">
-          <span>回复: @xxxx</span>
+          <span>回复: @{{ replyComment.user.name }}</span>
+          <span 
+            class="cancel-reply" 
+            @click="cancelReply">取消回复</span>
         </div>
         <div class="reply-content">
-          内容
+          {{ replyComment.content }}
         </div>
       </div>
       <div>
@@ -78,6 +95,9 @@ export default {
   computed: {
     list() {
       return this.$store.state.comment.list
+    },
+    replyComment() {
+      return this.list.data.find(comment => Object.is(comment._id, this.pid))
     }
   },
 
@@ -98,6 +118,22 @@ export default {
     // 回复
     reply(pid) {
       this.pid = pid
+    },
+    // 取消回复
+    cancelReply() {
+      this.pid = null
+    },
+    // 找到回复来源 type: 1 name | 2 内容
+    findReplyParent(id, type) {
+      const parent = this.list.data.find(comment => Object.is(comment._id, id))
+      if(!!parent) {
+        if(type === 1) {
+          return parent.user ? parent.user.name : null
+        } else if (type === 2) {
+          return parent.content
+        }
+      }
+      return null
     },
     // 提交评论
     submit() {
@@ -131,6 +167,16 @@ export default {
     border-bottom: 1px dashed #ccc;
     .content {
       padding: 12px 0;
+      .reply {
+        padding: 8px 8px 12px;
+        border-left: 3px solid #e2e2e2;
+        margin-bottom: 8px;
+        .reply-user {
+          font-style: italic;
+          margin-bottom: 8px;
+          color: #777;
+        }
+      }
     }
   }
 }
@@ -138,7 +184,7 @@ export default {
   .user {
     display: flex;
     justify-content: space-between;
-    padding-bottom: 12px;
+    margin-bottom: 16px;
     input {
       &.name {
         max-width: 150px;
@@ -149,6 +195,32 @@ export default {
       &:last-child {
         margin-right: 0;
       }
+    }
+  }
+  .reply {
+    margin-bottom: 16px;
+    .reply-user {
+      display: flex;
+      margin-bottom: 12px;
+      span {
+        &:first-child {
+          margin-right: 12px;
+          flex: 1;
+        }
+        &.cancel-reply {
+          cursor: pointer;
+        }
+        padding: 8px;
+        border: 1px solid #ccc;
+        border-radius: 4px;
+        background: #f2f2f2;
+      }
+    }
+    .reply-content {
+      padding: 8px;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      background: #f2f2f2;
     }
   }
   .tools {
