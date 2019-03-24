37d104a4ffed9c88a0c6319c471227bed80724ec
wyq-mac <wangyaqi.chn@gmail.com>
2019-03-24 11:24:36 +0800

    备案号

--- components/footer.vue
+++ components/footer.vue
@@ -2,6 +2,10 @@
   <footer>
     <span>Powered by Nuxt.js、</span>
     <span>Design by <a href="#">kkfor</a></span>
+    <span><a 
+      href="http://www.miitbeian.gov.cn" 
+      target="_blank"
+      rel="nofollow">冀ICP备18008392号</a></span>
   </footer>
 </template>
 <script>
