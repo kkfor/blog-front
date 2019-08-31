4e9c2c05f80da8e80acf1035fe44cc35c12fd864
wyq-mac <wangyaqi.chn@gmail.com>
2019-08-31 18:37:15 +0800

    update: config

--- config/index.js
+++ config/index.js
@@ -3,6 +3,5 @@ const isDev = process.env.NODE_ENV === 'development'
 export default {
   baseURL: isDev
     ? 'http://localhost:5000/'
-    : // 'https://api.kkfor.com/api' :
-      'https://api.kkfor.com/api'
+    : 'https://api.kkfor.com/'
 }
